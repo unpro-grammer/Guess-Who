8d443549018444b3db14706eb76271384f191e73
Jacky Zhang <ajhz488@aucklanduni.ac.nz>
2024-09-12 23:28:00 +1200

    Added highlight feature when hovering over suspects

--- src/main/java/nz/ac/auckland/se206/controllers/GuessingController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GuessingController.java
@@ -1,8 +1,11 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 
 public class GuessingController {
@@ -11,4 +14,10 @@ public class GuessingController {
   @FXML Rectangle scholar;
   @FXML TextArea explainationField;
   @FXML Button confimGuessingButton;
+
+  @FXML
+  private void confirmGuessing(ActionEvent event) throws IOException {}
+
+  @FXML
+  private void selectSuspect(MouseEvent event) throws IOException {}
 }
