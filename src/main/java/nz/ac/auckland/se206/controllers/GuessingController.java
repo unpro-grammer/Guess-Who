0f8ca176b40d3c2b85f8e5729a83e3673586dcae
Jacky Zhang <ajhz488@aucklanduni.ac.nz>
2024-09-12 15:18:11 +1200

    Created controller for Guessing scene, modified switching to Guessing state

---
+++ src/main/java/nz/ac/auckland/se206/controllers/GuessingController.java
@@ -0,0 +1,14 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.shape.Rectangle;
+
+public class GuessingController {
+  @FXML Rectangle leadScientist;
+  @FXML Rectangle labTechnician;
+  @FXML Rectangle scholar;
+  @FXML TextArea explainationField;
+  @FXML Button confimGuessingButton;
+}
