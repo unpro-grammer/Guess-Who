e6bf3a57356aafb2d75db8243630306be2391d04
Grace <gxu660@aucklanduni.ac.nz>
2024-09-15 22:58:09 +1200

    Add turning effect of scientist on hover

--- src/main/java/nz/ac/auckland/se206/controllers/LeadScientistController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LeadScientistController.java
@@ -1,3 +1,26 @@
 package nz.ac.auckland.se206.controllers;
 
-public class LeadScientistController extends RoomController {}
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class LeadScientistController extends RoomController {
+  @FXML private ImageView leadscientist;
+  @FXML private ImageView leadscientistturned;
+
+  @FXML
+  public void initialize() {
+    hideTurned();
+  }
+
+  @FXML
+  void showTurned() {
+    leadscientist.setVisible(false);
+    leadscientistturned.setVisible(true);
+  }
+
+  @FXML
+  void hideTurned() {
+    leadscientist.setVisible(true);
+    leadscientistturned.setVisible(false);
+  }
+}
